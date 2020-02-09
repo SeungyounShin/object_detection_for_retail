dfc7e5a8f5a461a88f4e51b09bd8bec53485e1c9
Seungyoun, Shin <ssy10011218@gmail.com>
2020-02-09 21:45:05 +0900

    detect
    center detect

--- detect.py
+++ detect.py
@@ -1,16 +1,20 @@
 import random
-import cv2,os
+import cv2,os,time
 import numpy as np
 import torch
 from nets.hourglass import get_hourglass
 import matplotlib.pyplot as plt
+from utils.image import transform_preds
 from skimage.transform import resize
+from utils.post_process import ctdet_decode, _nms, _topk
 
-random.seed(None)
+random.seed(a=None)
 
 model_path  = "./checkpoints/140.pth"
 train_img_dir = os.listdir("./data/retail/images")
-images_path = "./data/retail/images/"+train_img_dir[random.randint(1,150)]
+#images_path = "./data/retail/images/"+train_img_dir[random.randint(1,150)]
+images_path = "./data/retail/images/"+train_img_dir[120]
+score_threshold = 0.9
 
 #load a image
 img = cv2.imread(images_path)
@@ -35,8 +39,15 @@ hmap, regs, w_h_, theta = zip(*outputs)
 fig = plt.figure(figsize=(12, 6))
 img = img.view(3,height,width).permute(1,2,0).numpy()
 plt.imshow(img)
-hmap = hmap[0].squeeze()/255.
+hmap = torch.sigmoid(hmap[-1])
+hmap = _nms(hmap)
+scores, inds, clses, ys, xs = _topk(hmap, K=100)
+select = scores > score_threshold
+ys, xs = ys[select], xs[select]
+hmap = hmap.squeeze()
+
 hmap = resize(hmap.detach().numpy(), (96*4, 128*4))
 plt.imshow(hmap, alpha = 0.5)
+plt.scatter(xs*4,ys*4,c='red')
 
 plt.show()
